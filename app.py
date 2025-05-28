dc49bd9186e80d41021f8ebf57f1c02738c7fab1
Tariq <tariq@movrs.com>
2025-05-28 18:47:14 +0500

    in progress

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from PyQt6.QtWidgets import (QApplication)
 import sys
 from GlassMorphicLogin import GlassMorphicLogin
+from movrs_apis import update_json_fields
 
 
 
 
 if __name__ == "__main__":
+    update_json_fields([['state', '']])
+
     app = QApplication(sys.argv)
     window = GlassMorphicLogin()
     window.show()
