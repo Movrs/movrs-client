b9e018d2d3327e5c3d6625220b4cfd58b1186b6d
abdullahchand <abdullah.chand96@gmail.com>
2025-10-13 21:22:07 +0500

    [Fix] Added fix for user cred json file

--- setup.py
+++ setup.py
@@ -2,21 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="movrs-client",
-    version="0.1.7",
+    version="0.1.8",
     description="MOVRS Client GUI and CLI",
     author="Tariq Sarfraz",
     packages=find_packages(),
-    install_requires=[
-        "PyQt6",
-        "requests",
-        "pyqt6_sip",
-        "pyyaml",
-        "requests"
-    ],
-    entry_points={
-        "console_scripts": [
-            "movrs-client=movrs_client.app:main"
-        ]
-    },
+    install_requires=["PyQt6", "requests", "pyqt6_sip", "pyyaml", "requests"],
+    entry_points={"console_scripts": ["movrs-client=movrs_client.app:main"]},
     include_package_data=True,
-) 
+)
+
